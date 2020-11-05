8f64b997f0f59cacecb1b2b0fa81426d14434cf4
Emmanuella <emmavduru@gmail.com>
2020-11-05 19:59:01 +0100

    A minor correction

--- client/modalScript.js
+++ client/modalScript.js
@@ -4,7 +4,7 @@ const nicknameInput = document.querySelector("#nickname-input");
 
 // Close the modal when nick-name is typed
 nicknameInput.onkeypress = (e) => {
-  let keycode = e.keycode ? e.keycode : e.which;
+  let keycode = e.keyCode ? e.keyCode : e.which;
   if (keycode === "13") {
     modal.style.display = "none";
   }
