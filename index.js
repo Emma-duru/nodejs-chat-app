292eb0619879f60f222ac523035c4702db1d3011
Emmanuella <emmavduru@gmail.com>
2020-11-05 19:04:36 +0100

    Instantiate socket.io and add event to listen for change in username

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //  requiring packages
 const express = require("express");
 const app = express();
-let randomColor = require("randomcolor");
+const randomColor = require("randomcolor");
 const uuid = require("uuid");
 
 // middleware
@@ -14,3 +14,36 @@ app.get("/", (req, res) => {
 
 // Listen on port 3000
 const server = app.listen(process.env.PORT || 3000);
+
+// socket.io instantiation
+const io = require("socket.io")(server);
+
+const users = [];
+const connections = [];
+
+// listen on every connection
+io.on("connection", (socket) => {
+  console.log("New user connected");
+  // add the new socket to the connections array
+  connections.push(socket);
+  // initialize a random color for the socket
+  const color = randomColor();
+
+  // Set the first username of the user as Anonymous
+  socket.username = "Anonymous";
+  socket.color = color;
+
+  // Listen on change_username
+  socket.on("change_username", (data) => {
+    const id = uuid.v4(); // create a random id for the user
+    socket.id = id;
+    socket.username = data.nickName;
+    users.push({ id, username: socket.username, color: socket.color });
+    updateUsernames();
+  });
+
+  // update Username in the client
+  const updateUsername = () => {
+    io.sockets.emit("get users", users);
+  };
+});
